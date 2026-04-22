0a49110edded5e4f42a2f37d48e280f6611c135c
klumaks <maxklubkov@gmail.com>
2026-04-22 23:43:09 +0300

    fix

--- main.js
+++
@@ -1,9 +0,0 @@
-import { MainPage } from "./pages/main/index.js";
-
-const appState = {
-    participants: null
-};
-
-const root = document.getElementById('root');
-const mainPage = new MainPage(root, appState);
-mainPage.render();
